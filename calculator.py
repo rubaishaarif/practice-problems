64247fb465e2f0c4ee33fbb675ded60f099916eb
rubaishaarif <rubaishaarif41@gmail.com>
2025-11-20 19:05:20 +0500

    Implement basic calculator functionality

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a = input("Enter 1st num : ")
+op = input("Enter operator : ")
+b = input("Enter 2nd num : ")
+if op == '+':
+    print(int(a) + int(b))
+elif op == '-':
+    print(int(a) - int(b))
+elif op == '*':
+    print(int(a) * int(b))
+elif op == '/':
+    print(int(a) / int(b))
